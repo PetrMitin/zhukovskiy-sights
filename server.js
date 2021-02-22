6eccec573d9e8cfe2ab64dbfbb6bd4dc91a985d2
Petr Mitin <petr09mitin@mail.ru>
2021-02-22 20:35:53 +0300

    Added cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const path = require('path')
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
+app.use(cors())
 app.use(express.static(path.resolve(__dirname, 'public')))
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
